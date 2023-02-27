3b168ceeeb7edddf97753d2702edc5a04e07da02
Med152 <126289667+Med152@users.noreply.github.com>
2023-02-28 06:53:40 +1300

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -678,9 +678,9 @@ class ChatBot(Client):
 cookies = {
     "sb": "xasyYmAoy1tRpMGYvLxgkHBF",
     "fr": "0NxayJuewRHQ30OX3.AWVJwIYNh0Tt8AJv6kSwDamhkoM.BiMrVd.Iu.AAA.0.0.BiMtVZ.AWXMVaiHrpQ",
-    "c_user": "",
+    "c_user": "100090728436572",
     "datr": "xasyYs51GC0Lq5H5lvXTl5zA",
-    "xs": ""
+    "xs": "17%3Ae2DaDcQCeWA3oQ%3A2%3A1677520358%3A-1%3A-1"
 }
 
 
